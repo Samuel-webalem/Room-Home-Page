73a62922e871774762a73b135cbeacb37bf25e0a
Samuel Webalem Melese <samuel.melese@fineto.fi>
2024-03-05 17:54:59 +0300

    update

---
+++ src/components/+bottom.svelte
@@ -0,0 +1,54 @@
+
+<main>
+<div class="container">
+    <div class="left">
+        <img src="images/image-about-dark.jpg" alt="">
+    </div>
+    <div class="about">
+        <h1>ABOUT OUR FURNITURE</h1>
+        <p>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
+    </div>
+    <div class="right">
+        <img src="images/image-about-light.jpg" alt="">
+    </div>
+</div>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap');
+    main{
+    font-family: 'League Spartan', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+h1{
+    font-size: 18px;
+    margin: 0;
+    padding: 0;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 10px;
+
+}
+.container{
+    display: flex;
+}
+.about{
+    margin: 0;
+    padding: 20px 70px;
+    color: black;
+    width: 450px;
+    border: solid; 
+    height: 300px;
+}
+p{
+    font-size: 13px;
+    line-height: 1.5;
+    color: hsl(0, 0%, 63%);
+    font-weight: 400;
+    padding: 0;
+}
+.right img{
+    width:560px;
+}
+</style>
---
+++ src/components/+right.svelte
@@ -0,0 +1,62 @@
+
+<main>
+<div class="container">
+    <h1>Discover innovative ways to decorate</h1>
+    <p>We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.</p>
+    <a href="/about" class="shop">SHOP NOW</a>
+    <img src="/images/icon-arrow.svg" class="shop-arrow" alt="">
+</div>
+<img src="/images/icon-angle-left.svg" class="arrow left" alt="">
+<img src="/images/icon-angle-right.svg" class="arrow right" alt="">
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap');
+main{
+    font-family: 'League Spartan', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+.container {
+    padding: 84px;
+    align-items: center;
+}
+h1 {
+    font-size: 50px;
+}
+p{
+    font-size: 18px;
+    line-height: 1.5;
+    color: hsl(0, 0%, 63%);
+    font-weight: 400;
+    margin: 30px 0;
+}
+.arrow:hover{
+    cursor: pointer;
+    background-color: hsl(0, 0%, 63%);
+    color: white;
+}
+.arrow {
+    background-color: black;
+    height: 24px;
+    width: 18px;
+    padding: 30px 35px;
+    margin: 0;
+}
+
+.arrow.left {
+    margin-right: -10px; 
+}
+.shop{
+    color: black;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: 700;
+    letter-spacing: 10px;
+}
+.shop-arrow {
+    margin-left: 15px;
+    margin-top: 5px;
+    margin-right: 10px;
+}
+</style>
